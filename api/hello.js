a14eca6ca3e8ad45f26503e0407da809b3014345
TARS-16 <pawannimbare15@gmail.com>
2026-04-28 04:23:41 +0530

    Add hello.js API file

---
+++ api/hello.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({});
+  }
+
+  return res.status(200).json({
+    message: "Hello from Vercel Cloud!",
+    cloud: "Vercel",
+    status: "Multi-cloud integration working!"
+  });
+}
